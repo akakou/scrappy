dbb4ac4c176929daf868ffaa5cd6b5ed7e1fe50a
akakou <akakou571@gmail.com>
2022-01-25 11:30:53 +0900

    [init] Docker environment

---
+++ container/run.sh
@@ -0,0 +1,4 @@
+mkdir -p /home/attestation/.config/google-chrome/NativeMessagingHosts
+cp /home/attestation/attestation/app/attestation.json /home/attestation/.config/google-chrome/NativeMessagingHosts/com.akakou.attestation.json
+chmod +x /home/attestation/.config/google-chrome/NativeMessagingHosts/com.akakou.attestation.json
+google-chrome --no-sandbox -user-data-dir="/home/attestation/.config/google-chrome"
