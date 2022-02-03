a27205ebc904ff3facb2cca0702c1cb5ac2d070c
akakou <akakou571@gmail.com>
2022-02-03 13:57:10 +0900

    [add] Attestation log model

---
+++ client/app/model.py
@@ -0,0 +1,105 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, Text, Float, DateTime
+from sqlalchemy.orm import scoped_session, sessionmaker
+from datetime import datetime
+import random
+import logging
+
+
+handler = logging.FileHandler('/attestation/ignored_workspace/client.log')
+handler.setLevel(logging.ERROR)
+logging.getLogger('sqlalchemy').addHandler(handler)
+
+
+MAX = 10
+TERM_LEN = 50
+
+ENGINE = create_engine('sqlite:////attestation/ignored_workspace/db.sqlite3', echo=False)
+Base = declarative_base()
+
+session = scoped_session(
+    sessionmaker(
+        bind = ENGINE,
+        autocommit=False,
+    )
+)
+
+ERRORS = {
+    "HAVE_USED_ALL_COUNTER": 1
+}
+
+
+# logger = logging.getLogger('sqlalchemy')
+# logger.propagate = False
+
+# handler = logging.FileHandler('/attestation/ignored_workspace/client.log')
+# handler.setLevel(logging.ERROR)
+# logger.addHandler(handler)
+
+
+class AttestationLog(Base):
+    __tablename__ = 'attestation_logs'
+
+    _id = Column('id', Integer, primary_key = True)
+    domain = Column('domain', Text)
+    counter = Column('counter', Text)
+    created_at = Column(DateTime, default=datetime.now)
+
+    def find_by_domain_and_datetime(domain, datetime):
+        attestation_logs = session.query(AttestationLog) \
+                .filter(AttestationLog.domain == domain, AttestationLog.created_at == datetime)
+
+        return attestation_logs
+
+    def select_counters_by_domain_and_datetime(domain, datetime):
+        attestation_logs = AttestationLog.find_by_domain_and_datetime(domain, datetime)
+        return list(map(lambda x: x.domain, attestation_logs))
+
+    def gen_new_counter(domain, now):
+        counters = AttestationLog.select_counters_by_domain_and_datetime(domain, now) 
+        
+        if len(counters) >= MAX - 1:
+            return None
+
+        while True:
+            counter = random.randint(1, MAX)
+            
+            if counter not in counters:
+                return counter
+        
+    def gen_attestation_log(domain):
+        now = datetime.now()
+        now = now.replace(hour=now.hour, minute=now.minute, second=0, microsecond=0)
+
+        attestation_log = AttestationLog()
+        attestation_log.domain = domain
+        attestation_log.created_at = now
+
+        attestation_log.counter = AttestationLog.gen_new_counter(domain, now)
+
+        if attestation_log.counter is None:
+            return None
+        
+        return attestation_log
+        
+    def save_attestation_log(attestation_log):
+        session.add(attestation_log)
+        session.commit()
+    
+    def __str__(self):
+        return f'{self.domain}@{self.created_at}@{self.counter}' 
+
+
+Base.metadata.create_all(ENGINE)
+
+if __name__ == '__main__':
+    MAX = 5
+
+    attestation_log = AttestationLog.gen_attestation_log('example.com')
+    print("--- attestation_log ---\n", attestation_log)
+
+    if attestation_log is not None:
+        AttestationLog.save_attestation_log(attestation_log)
+
+
+
