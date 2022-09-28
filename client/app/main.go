904ad286ebf672b456e18cd406ec26f68ef3b2c0
akakou <akakou571@gmail.com>
2022-09-28 15:35:23 +0900

    [add] go extension binary

--- client/app/main.go
+++ client/app/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/binary"
+	"io"
+	"os"
+)
 
+func main() {
+	var inLength uint32
+
+	stdin := bufio.NewReader(os.Stdin)
+	binary.Read(stdin, binary.LittleEndian, &inLength)
+	buf := make([]byte, inLength)
+	io.ReadFull(stdin, buf)
+
+	payload := []byte("{\"signature\":\"signature\",\"count\":0}")
+
+	stdout := bufio.NewWriter(os.Stdout)
+	outLength := len(payload)
+
+	binary.Write(stdout, binary.LittleEndian, int32(outLength))
+	for head := 0; head < outLength; {
+		n, _ := stdout.Write(payload[head:])
+		head += n
+	}
+
+	stdout.Flush()
 }
--- client/extension/background.js
+++ client/extension/background.js
@@ -1,12 +1,12 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     chrome.runtime.sendNativeMessage(
         "com.akakou.attestation",
-        { nonce: request.nonce, origin: sender.origin },
+        { nonce: 'request.nonce', origin: 'sender.origin' },
         (response) => {
             console.log("Messaging host sais: ", response);
             console.log("ERROR: ", chrome.runtime.lastError);
 
-            sendResponse(response);
+            // sendResponse(response);
         });
 
     return true;
