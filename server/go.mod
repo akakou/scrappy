aa5204305de1968d1d4ef17ba048c8c3aaa8499d
akakou <akakou571@gmail.com>
2022-10-03 17:18:59 +0900

    [add] add/get session

--- server/go.mod
+++ server/go.mod
@@ -8,6 +8,7 @@ require miracl v0.0.0 // indirect
 
 require (
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/gin-contrib/sessions v0.0.5 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -15,6 +16,9 @@ require (
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/google/go-tpm v0.3.3 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- server/main.go
+++ server/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	secret := []byte("secret")
 	r := gin.Default()
+
+	store := cookie.NewStore(secret)
+	r.Use(sessions.Sessions("mysession", store))
+
 	r.LoadHTMLGlob("templates/*.html")
 
 	r.GET("/", func(c *gin.Context) {
+		period := time.Now()
+		period = time.Date(period.Year(), period.Month(), period.Day(), period.Hour(), period.Minute(), 0, 0, time.UTC)
+
+		session := sessions.Default(c)
+
+		session.Set("period", period.Unix())
+		session.Save()
+
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
@@ -24,9 +40,13 @@ func main() {
 	})
 
 	r.POST("/slow_with_attest", func(c *gin.Context) {
+		session := sessions.Default(c)
+		period := session.Get("period")
+
 		attestation := c.PostForm("attestation")
 
 		fmt.Printf("attestation=%v\n", attestation)
+		fmt.Printf("period=%v\n", period)
 
 		c.HTML(http.StatusOK, "hello.html", gin.H{})
 	})
