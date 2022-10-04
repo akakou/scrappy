c48db23ef428939cbfd0b7c1f1ab118e25d56122
akakou <akakou571@gmail.com>
2022-10-04 17:00:25 +0900

    [add] build go programms and run server

--- container/run.sh
+++ container/run.sh
@@ -1,18 +1,26 @@
+# rm -rf /home/attestation/.config
+mkdir -p /home/attestation/.config
+
 mkdir -p /home/attestation/.config/google-chrome/NativeMessagingHosts/
 cp /attestation/client/app/attestation.json /home/attestation/.config/google-chrome/NativeMessagingHosts/com.akakou.attestation.json
-chown attestation /home/attestation/.config/google-chrome/NativeMessagingHosts/com.akakou.attestation.json
+chown attestation -R /home/attestation/.config/
 
 ### build
 export PATH=$PATH:/usr/local/go/bin
+cd /attestation/client/app
+go build 
 
+### init
+cd /attestation/init
+go build 
+./m
 
 ### Chrome
 chown attestation /dev/tpm0
 chown attestation -R /attestation/
 
 cd /attestation/
-su attestation -c 'google-chrome --no-sandbox -user-data-dir="/home/attestation/.config/google-chrome" http://localhost:5000'
+su attestation -c 'google-chrome --no-sandbox -user-data-dir="/home/attestation/.config/google-chrome" http://localhost:8080  &'
 
-cd /attestation/server/templates
-python3 -m http.server 5000
-
+cd /attestation/server/
+go build && go run main.go
