f67d18ae605cb71df2cca167f385652e7d6d726e
akakou <akakou571@gmail.com>
2022-10-04 10:47:47 +0900

    [add] verify ecdaa signature

--- server/main.go
+++ server/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/akakou/ecdaa"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
+type Config struct {
+	Cred   *ecdaa.MiddleEncodedCredential
+	Isk    *ecdaa.MiddleEncodedISK
+	Ipk    *ecdaa.MiddleEncodedIPK
+	Handle []byte
+}
+
 func main() {
+	var config Config
+	configBuf, err := ioutil.ReadFile("../init/config.json ")
+
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
+	err = json.Unmarshal(configBuf, &config)
+
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
 	secret := []byte("secret")
 	r := gin.Default()
 
@@ -40,13 +65,37 @@ func main() {
 	})
 
 	r.POST("/slow_with_attest", func(c *gin.Context) {
+		var signature ecdaa.MiddleEncodedSignature
 		session := sessions.Default(c)
-		period := session.Get("period")
+		period := session.Get("period").(string)
 
 		attestation := c.PostForm("attestation")
 
+		attestBuf, err := base64.StdEncoding.DecodeString(attestation)
+		if err != nil {
+			log.Fatal("error:", err)
+			c.HTML(http.StatusOK, "err.html", gin.H{})
+		}
+
+		err = json.Unmarshal(attestBuf, &signature)
+
+		if err != nil {
+			log.Fatal("error:", err)
+		}
+
 		fmt.Printf("attestation=%v\n", attestation)
 		fmt.Printf("period=%v\n", period)
+
+		err = ecdaa.Verify(
+			[]byte{},
+			[]byte(period),
+			signature.Decode(),
+			config.Ipk.Decode(),
+		)
+
+		if err != nil {
+			c.HTML(http.StatusOK, "err.html", gin.H{})
+		}
 
 		c.HTML(http.StatusOK, "hello.html", gin.H{})
 	})
