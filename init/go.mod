ee324f3c2de57945141f0ba924abd8fe63532efd
akakou <akakou571@gmail.com>
2022-10-04 12:10:52 +0900

    [add] support core package

--- init/go.mod
+++ init/go.mod
@@ -2,19 +2,21 @@ module example.com/m/v2
 
 go 1.18
 
-require github.com/akakou/ecdaa v0.0.0
+require github.com/akakou/ecdaa v0.0.0 // indirect
+
+require core v0.0.0
 
 require miracl v0.0.0 // indirect
 
 require (
-	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/google/go-tpm v0.3.3 // indirect
-	github.com/x448/float16 v0.8.4 // indirect
-	golang.org/x/sys v0.0.0-20210629170331-7dc0b73dc9fb // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 )
 
 replace github.com/akakou/ecdaa => ../thirdparty/ecdaa
 
 replace miracl => ../thirdparty/ecdaa/thirdparty/miracl
+
+replace core => ../core
 
 replace github.com/google/go-tpm => ../thirdparty/ecdaa/thirdparty/go-tpm
--- init/main.go
+++ init/main.go
@@ -1,91 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"core"
 	"log"
-
-	"github.com/akakou/ecdaa"
 )
 
-const TPM_PATH = "/dev/tpm0"
+func main()  {
+	err := core.Setup()
 
-type Config struct {
-	Cred   *ecdaa.MiddleEncodedCredential
-	Isk    *ecdaa.MiddleEncodedISK
-	Ipk    *ecdaa.MiddleEncodedIPK
-	Handle []byte
+	if err != nil {
+		log.Fatalf("%v: ", err)
+	}
 }
 
-func main() {
-	password := []byte("piyo")
-
-	rng := ecdaa.InitRandom()
-
-	tpm, err := ecdaa.OpenTPM(password, TPM_PATH)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	issuer := ecdaa.RandomIssuer(rng)
-
-	err = ecdaa.VerifyIPK(&issuer.Ipk)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	seed, issuerSession, err := issuer.GenSeedForJoin(rng)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	member := ecdaa.NewMember(tpm)
-	req, memberSession, err := member.GenReqForJoin(seed, rng)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	tpm.Close()
-
-	cipherCred, err := issuer.MakeCred(req, issuerSession, rng)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	tpm, err = ecdaa.OpenTPM(password, TPM_PATH)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	tmp := member
-	member = ecdaa.NewMember(tpm)
-	member.KeyHandles = tmp.KeyHandles
-
-	cred, err := member.ActivateCredential(cipherCred, memberSession, &issuer.Ipk)
-
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	credBin := cred.Encode()
-
-	iskBin := issuer.Isk.Encode()
-	ipkBin := issuer.Ipk.Encode()
-	handle := member.KeyHandles.Handle.Name.Buffer
-
-	config := Config{
-		Cred:   credBin,
-		Isk:    iskBin,
-		Ipk:    ipkBin,
-		Handle: handle,
-	}
-
-	buf, err := json.Marshal(&config)
-
-	if err != nil {
-		log.Fatalf("err: %v", err)
-	}
-
-	fmt.Printf("%s", buf)
-
-}
