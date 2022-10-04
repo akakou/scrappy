ded209297c2054eb4a13c464fc0e7ef73c742c09
akakou <akakou571@gmail.com>
2022-10-04 17:02:35 +0900

    [fix] send correct origin and period

--- client/extension/background.js
+++ client/extension/background.js
@@ -1,12 +1,14 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     chrome.runtime.sendNativeMessage(
         "com.akakou.attestation",
-        { nonce: 'request.nonce', origin: sender.origin },
+        { origin: sender.origin, period: Number(request.period) },
         (response) => {
+            console.log("origin: ", sender.origin)
+            console.log("period: ", sender.period)
             console.log("Messaging host sais: ", response);
             console.log("ERROR: ", chrome.runtime.lastError);
 
-            // sendResponse(response);
+            sendResponse(response);
         });
 
     return true;
