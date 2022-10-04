8e6e22c6d4a4c10bf21f2e2aed479be548c72396
akakou <akakou571@gmail.com>
2022-10-04 17:00:49 +0900

    [add] some value to template
    - error message
    - period

--- server/main.go
+++ server/main.go
@@ -18,6 +18,8 @@ type Config struct {
 	Handle []byte
 }
 
+const HOST_NAME = "http://localhost:8080"
+
 func main() {
 	secret := []byte("secret")
 	r := gin.Default()
@@ -30,13 +32,16 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		period := time.Now()
 		period = time.Date(period.Year(), period.Month(), period.Day(), period.Hour(), period.Minute(), 0, 0, time.UTC)
+		unixPeriod := period.Unix()
 
 		session := sessions.Default(c)
 
-		session.Set("period", period.Unix())
+		session.Set("period", unixPeriod)
 		session.Save()
 
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"period": unixPeriod,
+		})
 	})
 
 	r.GET("/fast", func(c *gin.Context) {
@@ -49,17 +54,19 @@ func main() {
 
 	r.POST("/slow_with_attest", func(c *gin.Context) {
 		session := sessions.Default(c)
-		period := session.Get("period").(string)
+		period := session.Get("period").(int64)
 
 		attestation := c.PostForm("attestation")
 
-		err := core.Verify(attestation, period)
+		err := core.Verify(attestation, HOST_NAME, int(period))
 
 		if err != nil {
-			c.HTML(http.StatusOK, "err.html", gin.H{})
+			c.HTML(http.StatusOK, "error.html", gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			c.HTML(http.StatusOK, "hello.html", gin.H{})
 		}
-
-		c.HTML(http.StatusOK, "hello.html", gin.H{})
 	})
 
 	r.Run()
