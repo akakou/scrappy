97ae7a4960ee7d267e9026d910c586ccefa3541c
akakou <akakou571@gmail.com>
2022-10-05 11:46:35 +0900

    [add] sign or verify with deplication check

--- core/main.go
+++ core/main.go
@@ -5,9 +5,37 @@ func Setup() {
 }
 
 func Sign(origin string, period int) (string, error) {
+	db, err := SetupDB(CLIENT_DB_PATH)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer db.Close()
+
+	err = InsertIfItDoesNotExist(db, origin, period)
+
+	if err != nil {
+		return "", err
+	}
+
 	return CryptoSign(origin, period)
 }
 
 func Verify(signature, origin string, period int) error {
+	db, err := SetupDB(CLIENT_DB_PATH)
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	err = InsertIfItDoesNotExist(db, origin, period)
+
+	if err != nil {
+		return err
+	}
+
 	return CryptoVerify(signature, origin, period)
 }
