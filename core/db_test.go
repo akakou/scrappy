9a21d19f77ac46d19bc8f8591c3e20fb3a682889
akakou <akakou571@gmail.com>
2022-10-05 11:28:02 +0900

    [add] test for functions around db

---
+++ core/db_test.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const TEST_DB_PATH = ":memory:"
+
+func TestDB(t *testing.T) {
+	origin := "aaa"
+	period := 1000
+
+	db, err := SetupDB(TEST_DB_PATH)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = InsertIfItDoesNotExist(db, origin, period)
+
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	hasExist, err := HasExist(db, origin, period)
+
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if !hasExist {
+		t.Fatalf("basename %s with period %d does not exists", origin, period)
+	}
+}
