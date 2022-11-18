cae235b70bdad85a0e6f8bacb769fc1eaa921c0e
akakou <akakou571@gmail.com>
2022-11-19 05:27:32 +0900

    [add] verify bench

--- core/bench_test.go
+++ core/bench_test.go
@@ -5,29 +5,36 @@ import (
 	"testing"
 )
 
-const BENCH_DB = "./%v_bench.db"
+const BENCH_DB_PATH = "./%v_%v_bench.db"
 const BENCH_ORIGIN = "www.example.com"
 
 const BENCH_UNIT = 10
+const BENCH_MAX = 100000
 
-const BENCH_MAX = 1000000
+type targetFunc = func(b *testing.B, db *DB)
 
-const TMP_K = "0000000000000000000000000000000000000000000000000000000000000000"
+func prepareDB(conf DB, entityType string, logSize int, value string) {
+	path := fmt.Sprintf(BENCH_DB_PATH, entityType, logSize)
+	db, err := SetupDB(conf, path)
 
-func prepareDB(logSize int) {
-	db_name := fmt.Sprintf(BENCH_DB, logSize)
-	SIGNER_DB_PATH = db_name
+	if err != nil {
+		panic(err)
+	}
 
-	db, err := SetupVerifierDB(db_name)
+	countQuery := fmt.Sprintf(`SELECT COUNT (*) FROM %v`, conf.Table)
+	deleteQuery := fmt.Sprintf("DELETE FROM %v", conf.Table)
+
+	if err != nil {
+		panic(err)
+	}
+
 	defer db.DB.Close()
 
 	if err != nil {
 		panic(err)
 	}
 
-	res, err := db.DB.Query(
-		`SELECT COUNT (*) FROM BASENAMES`,
-	)
+	res, err := db.DB.Query(countQuery)
 
 	if err != nil {
 		panic(err)
@@ -35,80 +42,185 @@ func prepareDB(logSize int) {
 
 	var count int
 
+	res.Next()
 	res.Scan(&count)
+	res.Next()
 
 	if count == logSize {
 		return
 	}
 
-	fmt.Print("Delete DB\n")
+	if count > logSize {
+		fmt.Printf("%v != %v\n", count, logSize)
 
-	_, err = db.DB.Exec("DROP TABLE basenames")
-	if err != nil {
-		panic(err)
-	}
+		fmt.Println("clear!")
 
-	db.DB.Close()
-
-	db, err = SetupVerifierDB(db_name)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for i := 0; i < logSize; i++ {
-		err := Insert(db, TMP_K)
-
+		_, err = db.DB.Exec(deleteQuery)
 		if err != nil {
 			panic(err)
 		}
 	}
+
+	fmt.Println("insert!")
+
+	for i := count; i < logSize; i++ {
+		err := Insert(db, value)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%v/%v\n", i, logSize)
+	}
+
+	db.DB.Close()
 }
 
-func benchmarkOfSignerLog(b *testing.B, logSize int) {
-	db, err := SetupVerifierDB(SIGNER_DB_PATH)
+func benchSign(b *testing.B, db *DB) {
+	period := Now()
+
+	h := fmt.Sprintf("%v_%v", BENCH_ORIGIN, period)
+
+	b.StartTimer()
+	_, err := Sign(BENCH_ORIGIN, period)
+	b.StopTimer()
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+
+	query := fmt.Sprintf("DELETE FROM %v WHERE %v = ?", SIGNER_DB_CONF.Table, SIGNER_DB_CONF.Column)
+	_, err = db.DB.Exec(query, h)
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+}
+
+func benchVerify(b *testing.B, db *DB) {
+	period := Now()
+
+	signature, err := Sign(BENCH_ORIGIN, period)
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+
+	b.StartTimer()
+	// err = Verify(signature, BENCH_ORIGIN, period)
+
+	hasExist, err := HasExist(db, signature)
+
+	if err != nil {
+		b.Fatalf("has exist: %v", err)
+	}
+
+	if hasExist {
+		b.Fatalf(HAS_EXIST_ERROR, signature)
+	}
+
+	b.StopTimer()
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+
+	K, err := GetK(signature)
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+
+	query := fmt.Sprintf("DELETE FROM %v WHERE %v = ?", VERIFIER_DB_CONF.Table, VERIFIER_DB_CONF.Column)
+	_, err = db.DB.Exec(query, K)
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+}
+
+func benchSearchSignerLog(b *testing.B, db *DB) {
+	b.StartTimer()
+
+	period := Now()
+
+	basename := fmt.Sprintf("%v_%v", BENCH_ORIGIN, period)
+
+	b.StartTimer()
+	hasExist, err := HasExist(db, basename)
+
+	if err != nil {
+		b.Fatalf("has exist: %v", err)
+	}
+
+	if hasExist {
+		b.Fatalf(HAS_EXIST_ERROR, basename)
+	}
+
+	b.StopTimer()
+}
+
+func benchSearchVerifierLog(b *testing.B, db *DB) {
+	period := Now()
+
+	signature, err := Sign(BENCH_ORIGIN, period)
+
+	if err != nil {
+		b.Fatalf("%v: ", err)
+	}
+
+	b.StartTimer()
+	// err = Verify(signature, BENCH_ORIGIN, period)
+
+	hasExist, err := HasExist(db, signature)
+
+	if err != nil {
+		b.Fatalf("has exist: %v", err)
+	}
+
+	if hasExist {
+		b.Fatalf(HAS_EXIST_ERROR, signature)
+	}
+
+	b.StopTimer()
+}
+
+func benchmarkOfLogCore(b *testing.B, logSize int, conf DB, path string, target targetFunc) {
+	db, err := SetupDB(conf, path)
 	defer db.DB.Close()
 
 	if err != nil {
 		b.Fatalf("%v: ", err)
 	}
 
-	for i := 0; i < logSize; i++ {
-		err := Insert(db, TMP_K)
-
-		if err != nil {
-			b.Fatalf("%v: ", err)
-		}
-	}
+	b.ResetTimer()
+	b.StopTimer()
 
 	for i := 0; i < b.N; i++ {
-		b.ResetTimer()
-		period := Now()
-
-		b.StartTimer()
-		signature, err := Sign(BENCH_ORIGIN, period)
-		b.StopTimer()
-
-		if err != nil {
-			b.Fatalf("%v: ", err)
-		}
-
-		K, err := GetK(signature)
-
-		if err != nil {
-			b.Fatalf("%v: ", err)
-		}
-
-		_, err = db.DB.Exec("DELETE FROM basenames WHERE K = ?", K)
-
-		if err != nil {
-			b.Fatalf("%v: ", err)
-		}
+		target(b, db)
 	}
-
 }
 
-func BenchmarkOfSignerLog(b *testing.B) {
+func benchmarkOfLog(b *testing.B, conf DB, entityType string, value string, target targetFunc) {
+	for i := 1; i < BENCH_MAX; i *= BENCH_UNIT {
+		fmt.Printf("set up db with %d records\n", i)
+		prepareDB(conf, entityType, i, value)
+	}
+
+	fmt.Println("Ready!!")
+	fmt.Println("Start!!")
+
+	for i := 1; i < BENCH_MAX; i *= BENCH_UNIT {
+		name := fmt.Sprintf("log_size(%v):%d", entityType, i)
+
+		b.Run(name, func(b *testing.B) {
+			path := fmt.Sprintf(BENCH_DB_PATH, entityType, i)
+			benchmarkOfLogCore(b, i, conf, path, target)
+		})
+	}
+}
+
+func BenchmarkOfLog(b *testing.B) {
 	fmt.Println("Ready...")
 
 	err := Setup()
@@ -116,20 +228,15 @@ func BenchmarkOfSignerLog(b *testing.B) {
 		b.Fatalf("%v: ", err)
 	}
 
-	for i := 1; i < BENCH_MAX; i *= BENCH_UNIT {
-		fmt.Printf("set up db with %d records\n", i)
-		prepareDB(i)
+	h := fmt.Sprintf("%v_%v", BENCH_ORIGIN, Now())
+
+	signature, err := Sign(BENCH_ORIGIN, Now())
+	if err != nil {
+		b.Fatalf("%v: ", err)
 	}
 
-	fmt.Println("Ready!!")
-	fmt.Println("Start!!")
+	K, _ := GetK(signature)
 
-	for i := 1; i < BENCH_MAX; i *= BENCH_UNIT {
-		name := fmt.Sprintf("log_size:%d", i)
-
-		b.Run(name, func(b *testing.B) {
-			benchmarkOfSignerLog(b, i)
-
-		})
-	}
+	benchmarkOfLog(b, SIGNER_DB_CONF, "signer", h, benchSearchSignerLog)
+	benchmarkOfLog(b, VERIFIER_DB_CONF, "verifier", K, benchSearchVerifierLog)
 }
