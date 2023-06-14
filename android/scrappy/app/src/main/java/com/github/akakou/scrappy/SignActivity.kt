3e0a4cb6c246baf85c786ac9b47f57b643d50022
akakou <akakou571@gmail.com>
2023-06-15 06:32:08 +0900

    [add] get value from browser

--- android/scrappy/app/src/main/java/com/github/akakou/scrappy/SignActivity.kt
+++ android/scrappy/app/src/main/java/com/github/akakou/scrappy/SignActivity.kt
@@ -1,11 +1,21 @@
 package com.github.akakou.scrappy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
+
+        val i = intent
+        val urlString = i.dataString
+
+        val uri = Uri.parse(urlString)
+
+        Toast.makeText(this, "hello, ${uri.getQueryParameter("test")}", Toast.LENGTH_LONG).show()
     }
 }
