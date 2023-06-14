ab85f0780351d0763cdca9d1f134aeaa3d15b891
akakou <akakou571@gmail.com>
2023-06-15 01:36:50 +0900

    [refactor] create static object of scrappy

--- android/scrappy/app/src/main/java/com/github/akakou/scrappy/MainActivity.kt
+++ android/scrappy/app/src/main/java/com/github/akakou/scrappy/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.github.akakou.scrappy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.view.View
 import android.widget.EditText
 import android_scrappy.Android_scrappy.androidJoin
-import kotlin.concurrent.thread
-import android.util.Base64
+import android_scrappy.Android_scrappy.androidSign
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+
+
+object Scrappy {
+    var config : ByteArray? = null
+    var protocol = "https://"
+
+    fun parseLibraryResponse(response: String) : ByteArray {
+        val jsonObject = JSONObject(response)
+        val status = jsonObject.getString("status").toString()
+        val base64Buffer = jsonObject.getString("buffer").toString()
+
+        val buffer = Base64.decode(base64Buffer, Base64.DEFAULT)
+
+        if(status != "ok") {
+            val msg = String(buffer, Charsets.UTF_8)
+            error("Scrappy not work: $msg")
+        }
+
+        return buffer
+    }
+
+    fun join(issuerDomain: String, ipk: ByteArray): ByteArray? {
+        val response = androidJoin("http://$issuerDomain", ipk)
+        config = parseLibraryResponse(response)
+
+        return config
+    }
+
+    fun sign(origin: String, unixTime: Long): String {
+        return androidSign(origin, unixTime, config)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
     var msg = ""
